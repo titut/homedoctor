1138a885f13e1a5c5a8b7d2ebefb57b60917974b
titut <conghoangbillnew@gmail.com>
2021-06-25 21:08:31 +0700

    initialize homedoctor

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Doctor' });
+});
+
+router.get('/about', function(req,res,next){
+  res.render('about');
+})
+
+router.get('/doctors', function(req,res,next){
+  res.render('doctors');
+})
+
+router.get('/news', function(req,res,next){
+  res.render('news');
+})
+
+router.get('/protect', function(req,res,next){
+  res.render('protect');
+})
+
+module.exports = router;
